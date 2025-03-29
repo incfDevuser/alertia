6eeee2cc137bf88d74b9e0a9d02c0165b2ec313c
Martin Gomez <mgomeznavarrocorp@gmail.com>
2025-03-28 22:16:01 -0300

    Setup Inicial

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
+})
